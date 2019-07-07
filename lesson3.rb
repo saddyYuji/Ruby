4c0a97c2b52f3f7fbf080010b3cbd7a897436ea7
saddyYuji <saddy.yuji@gmail.com>
2019-07-07 19:51:56 +0900

    [Finish] 2-4

---
+++ lesson3.rb
@@ -0,0 +1,6 @@
+puts "WEBCAMP".length #文字数を返す
+# puts 100.length #NoMethodError、Integerにlengthメソッドはない
+puts "WEBCAMP".reverse #文字列を逆にする
+puts "WEBCAMPでプログラミング学習".include?("WEBCAMP") #含まれているか確認、戻り値はt/f
+puts "webcamp".upcase #大文字化
+puts "WEBCAMP".downcase #小文字化
